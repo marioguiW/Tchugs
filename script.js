e4c86ccf87345bc7f5fc13a9e2c7e9cef9fed689
Mario Guilherme Wilhelms <marioguilhermew@gmail.com>
2023-10-08 23:03:23 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,11 @@
+function show(){
+    console.log("teste")
+    document.querySelector('.hamburger').classList.toggle('open')
+    document.querySelector('.navigation').classList.toggle('active')
+}
+
+
+function scrolling(){
+    document.querySelector('.hamburger').classList.toggle('open')
+    document.querySelector('.navigation').classList.toggle('active')
+}
